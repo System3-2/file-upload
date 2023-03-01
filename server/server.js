7ce9ca0252d8b7420cff6b251180e7dcaddd89aa
OlojaMuyiwa <olojam4969@gmail.com>
2023-03-01 19:55:06 +0100

    removed postgres pool configuration

--- server/server.js
+++ server/server.js
@@ -1,24 +1,7 @@
 const express = require("express");
 const app = express();
 const { cloudinary } = require("./utils/cloudinary");
-const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: "oloja",
-  password: "Wardprowse26",
-  host: "localhost",
-  database: "image",
-  port: 5432,
-});
-
-pool
-  .connect()
-  .then(() => {
-    console.log(`connected`);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
@@ -27,12 +10,8 @@ app.get("/images", async function (req, res) {
   const resources = await cloudinary.v2.search
     .expression("resource_type:image")
     .sort_by("public_id", "desc")
-    .max_results(30)
     .execute()
-    // .then((result) => {
-    //   console.log(result);
-    // });
-
+  
     const result = resources.resources;
       res.send(result);
 });
